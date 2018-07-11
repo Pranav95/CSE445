99e4464be29d1ad9ef440f6825e07b003079f0ff
Pranav Havanurkar <pgodse95@gmail.com>
2018-07-11 10:27:05 -0700

    Added new project
    Code to encrypt and decrpyt files. Provided as a service

---
+++ Encrypt_Decrypt/Encrypt_Decrypt/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypt_Decrypt
+{
+    public class Cryption
+    {
+        public static string Encrypt(string plainString)
+        {
+            byte[] encryptedBytes = Encoding.ASCII.GetBytes(plainString);
+            string encrypted = "";
+
+            foreach (byte digit in encryptedBytes)
+            {
+                encrypted += digit;
+            }
+
+            return encrypted;
+        }
+        public static string Decrypt(string encryptedString)
+        {
+            string decrypted = "";
+            IEnumerable<string> arr = Enumerable.Range(0, encryptedString.Length / 3).Select(i => encryptedString.Substring(i * 3, 3));
+            List<byte> byteArr = new List<byte>();
+            string[] strArr = arr.ToArray();
+
+            List<int> list = new List<int>();
+            foreach (string k in strArr)
+            {
+                list.Add(Int32.Parse(k));
+            }
+            int[] myInts = list.ToArray();
+
+            foreach (int num in myInts)
+            {
+                byteArr.Add((byte)num);
+            }
+
+            decrypted = Encoding.UTF8.GetString(byteArr.ToArray<byte>());
+            return decrypted;
+        }
+    }
+}
